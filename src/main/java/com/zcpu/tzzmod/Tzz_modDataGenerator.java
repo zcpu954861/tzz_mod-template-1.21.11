8b1d78f45da98881d147920509e91f2d102b2668
zcpu <1762401537@qq.com>
2026-03-04 16:05:59 +0800

    Initial commit

---
+++ src/main/java/com/zcpu/tzzmod/Tzz_modDataGenerator.java
@@ -0,0 +1,11 @@
+package com.zcpu.tzzmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Tzz_modDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
