04331a89451e7d2ba03488a3907936869a0e3854
zcpu <1762401537@qq.com>
2026-04-25 01:34:42 +0800

    chore: add initial data generation providers

--- src/main/java/com/zcpu/tzzmod/Tzz_modDataGenerator.java
+++ src/main/java/com/zcpu/tzzmod/Tzz_modDataGenerator.java
@@ -1,5 +1,6 @@
 package com.zcpu.tzzmod;
 
+import com.zcpu.tzzmod.datagen.TzzModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import org.jspecify.annotations.NonNull;
@@ -7,6 +8,7 @@ import org.jspecify.annotations.NonNull;
 public class Tzz_modDataGenerator implements DataGeneratorEntrypoint {
     @Override
     public void onInitializeDataGenerator(@NonNull FabricDataGenerator fabricDataGenerator) {
-
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+        pack.addProvider(TzzModelProvider::new);
     }
 }
